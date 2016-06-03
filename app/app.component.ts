8720439c4691ce242297de2fe7aa2104127fe0fa
Ignacio Mieites <ia.mieites@gmail.com>
2016-06-03 18:19:14 -0300

    dashboard working with heroes detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,24 @@ import {Component} from '@angular/core';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import {DashboardComponent} from "./dashboard.component";
+import { HeroDetailComponent } from './hero-detail.component';
 
 @RouteConfig([
   {
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },{
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 
@@ -15,8 +27,11 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
   selector: 'my-app',
   template: `
      <h1>{{title}}</h1>
-     <a [routerLink]="['Heroes']">Heroes</a>
-     <router-outlet></router-outlet>
+     <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
   providers: [ ROUTER_PROVIDERS, HeroService ]
