afdeba9e2cb29f18ebafc48f1843049d04bc075d
Ignacio Mieites <ia.mieites@gmail.com>
2016-06-06 15:06:27 -0300

    dashboard and nav buttons added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,41 +1,36 @@
 import {Component} from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-import {DashboardComponent} from "./dashboard.component";
-import { HeroDetailComponent } from './hero-detail.component';
-
-@RouteConfig([
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },{
-    path: '/detail/:id',
-    name: 'HeroDetail',
-    component: HeroDetailComponent
-  }
-])
 
 @Component({
   selector: 'my-app',
   template: `
      <h1>{{title}}</h1>
      <nav>
-      <a [routerLink]="['Dashboard']">Dashboard</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
+       <a [routerLink]="['Dashboard']">Dashboard</a>
+       <a [routerLink]="['Heroes']">Heroes</a>
+     </nav>
+     <router-outlet></router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
   providers: [ ROUTER_PROVIDERS, HeroService ]
 })
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
 
 export class AppComponent {
   title = 'Tour of Heroes';
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,22 +1,7 @@
 import { Component } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  // notese que usa el
-  templateUrl: 'app/dashboard.component.html',
+  template: '<h3>My Dashboard</h3>'
 })
-export class DashboardComponent implements OnInit {
-  heroes: Hero[] = [];
-
-  constructor(private heroService: HeroService) {}
-
-  ngOnInit(){
-    this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1,5));
-  }
-
-  gotoDetail(){ /* not implemented yet */}
-}
+export class DashboardComponent { }
